0a402ec9a31f43b4235c81d0b0e541140d6a0c96
Ivan <v41new@gmail.com>
2020-11-05 22:44:19 +0300

    1

--- Test/js/script.js
+++ Test/js/script.js
@@ -1,27 +1,66 @@
 'use strict';
+// =================================CONTACT WITH USER
+// alert('Hello');
 
-let number = 4.6;
+// const result = confirm('are you here?');
+// console.log(result);
 
-console.log(-4 / 0);
-console.log('string' * 9);
+// const answer = prompt('Have 18?', '18');
+// console.log(typeof(answer)); //type
 
-const persone = '5';
+// const answer1 = prompt('Have 18?', '18');
+// console.log(answer1 + 5); //string + number
 
-const bool = false;
+// const answer2 = +prompt('Have 18?', '18');
+// console.log(answer2 + 5); //number + number
 
-//console.log(something);
+// const answers = []; // mas of answers
 
-let und;
-console.log(und);
+// answers[0] = prompt('name?', '');
+// answers[1] = prompt('lastname?', '');
+// answers[2] = prompt('age?', '');
 
-const obj = {
-    name: 'Ivan',
-    age: 25,
-    isMarried: false
-};
+// console.log(typeof(answers));
+// console.log(typeof(null));
 
-console.log(obj.name);
+// =================================INTERPOLATION
 
+// const category = 'toys';
 
-let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
-console.log(arr[1]);
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Hello, ${user}`);
+
+// =================================OPERATORS
+
+// console.log('arr' + ' - object'); // arr - object
+// console.log(4 + ' - object'); // 4 - object
+// console.log(4 + '5'); // 45
+// console.log(4 + +'5'); // 9
+
+let incr = 10,
+    decr = 10;
+
+// incr++; //post_inkrement
+// decr--; //post_dekrement
+// ++incr; //pred_inkrement
+// --decr; //pred_dekrement
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 == 8); // true
+// console.log(2*4 == '8'); // true
+// console.log(2*4 === '8'); // false - strogo
+// console.log(2*4+1 != 8); // true
+// console.log(2*4+1 !== 8); // true
+
+// const isChecked = true,
+//     isClose = false;
+
+// console.log(isChecked && isClose); //false
+// console.log(isChecked || isClose); //true
+// console.log(!isChecked || isClose); //false
